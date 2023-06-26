9f6c9e22f0a3790983c467fd14fe024bbddee85c
Ziyaan Khadim <ziyaankhadim@gmail.com>
2023-06-26 15:21:20 +0500

    add coding challenge #1

---
+++ Fundamentals-Part-1/coding challenge.js
@@ -0,0 +1,11 @@
+//coding challenge #1
+const weightMark = 78;
+const heightMark = 1.69;
+const weightJohn = 95;
+const heightJohn = 1.88;
+let markBMI = weightMark / heightMark ** 2;
+console.log(markBMI);
+let johnBMI = weightJohn / heightJohn ** 2;
+console.log(johnBMI);
+markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
