27e63364f4136fed9b0bbee9a6c43158cabc2409
Ziyaan Khadim <ziyaankhadim@gmail.com>
2023-06-26 15:19:21 +0500

    add data types

--- Fundamentals-Part-1/assignment.js
+++ Fundamentals-Part-1/assignment.js
@@ -3,3 +3,8 @@ let country = "Pakistan";
 let continent = "Asia";
 let population = 231.4;
 console.log(country, continent, population);
+
+// data types
+let isIsland = false;
+let language;
+console.log(isIsland, population, country, language);
