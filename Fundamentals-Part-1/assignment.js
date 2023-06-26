7ed6a521222f1d5a11ee5555c90c3f56c8ba6dc2
Ziyaan Khadim <ziyaankhadim@gmail.com>
2023-06-26 15:20:34 +0500

    add basic operators

--- Fundamentals-Part-1/assignment.js
+++ Fundamentals-Part-1/assignment.js
@@ -11,3 +11,13 @@ console.log(isIsland, population, country, language);
 
 // let, const, and var
 language = "URDU";
+
+//basic operators
+console.log(population / 2);
+console.log(population++);
+const finlandPopulation = 6;
+console.log(population > finlandPopulation);
+const avgPopulation = 33;
+console.log(population < avgPopulation);
+let description =
+  "Portugal is in Europe,and its 11 million people speak portuguese";
