19395d25d95d948fb6c5dbf3bf41ad8d09df728f
Ziyaan Khadim <ziyaankhadim@gmail.com>
2023-06-26 15:21:47 +0500

    add coding challenge #2

--- Fundamentals-Part-1/coding challenge.js
+++ Fundamentals-Part-1/coding challenge.js
@@ -9,3 +9,10 @@ let johnBMI = weightJohn / heightJohn ** 2;
 console.log(johnBMI);
 markHigherBMI = markBMI > johnBMI;
 console.log(markHigherBMI);
+
+//coding challenge #2
+if (markBMI > johnBMI) {
+  console.log(`Mark's BMI (${markBMI}) is higher than John's! (${johnBMI})`);
+} else {
+  console.log(`John's BMI ${johnBMI}) is higher than Mark's! (${markBMI})`);
+}
