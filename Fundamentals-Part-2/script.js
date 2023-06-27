f87ba1ac684dbab15cee06c21ddb4e4aadc751aa
Ziyaan Khadim <ziyaankhadim@gmail.com>
2023-06-27 10:09:07 +0500

    do coding challenge #2 Part 2

--- Fundamentals-Part-2/script.js
+++ Fundamentals-Part-2/script.js
@@ -22,3 +22,20 @@ function checkWinner(avgDolphins, avgKoalas) {
 
 checkWinner(avgDolphins1, avgKoalas1);
 checkWinner(avgDolphins2, avgKoalas2);
+
+//coding challenge #2
+
+function calcTip(bill) {
+  if (bill > 50 && bill < 300) {
+    return (bill / 100) * 15;
+  } else {
+    return (bill / 100) * 20;
+  }
+}
+console.log(calcTip(100));
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(tips);
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(total);
